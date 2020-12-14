c9aa6799b3d4271f42728abf0bef4708d469aff3
Thomas Woods <twoods@spartaglobal.com>
2020-12-14 16:41:05 +0000

    Even better code

--- greetings.py
+++ greetings.py
@@ -1,2 +1,16 @@
+for i in range(5):
+    print("Hello!")
+
 def twoods_amazing_function(name):
+    print(f"Hello {name}!")
+    if name == "Thomas":
+        print("That's a pretty cool name")
+    else:
+        print("That's a pretty cool name as well")
     print(f"This is the best function in the world, {name} is AMAZING!!")
+
+twoods_amazing_function("Thomas")
+
+
+
+
